048ccd599eaf36bc076419bf742f65acf03ff418
pangvelop <kgo103@naver.com>
2025-07-16 20:50:51 +0900

    refactor: controller단 이름 변경

--- src/main/java/com/board/board/controller/PostController.java
+++ src/main/java/com/board/board/controller/PostController.java
@@ -1,15 +1,10 @@
 package com.board.board.controller;
 
 import com.board.board.domain.Post;
-import com.board.board.domain.request.PostRequestDto;
-import com.board.board.domain.response.PostResponseDto;
 import com.board.board.service.PostService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -21,13 +16,13 @@ public class PostController {
     // 게시글 다건 조회
     // DTO로 바꾸자
     @GetMapping
-    public List<Post> list() {
+    public List<Post> lists() {
         return postService.getPosts();
     }
 
     // 게시글 단건 조회
     @GetMapping("/{id}")
-    public Post detail(@PathVariable Long id) {
+    public Post list(@PathVariable Long id) {
         return postService.getPost(id);
     }
 
